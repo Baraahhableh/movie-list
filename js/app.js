e891e75485b189bd4c97b337695b4bd6e631edcc
baraahhableh <baraah.hableh@yahoo.com>
2021-08-24 12:21:34 +0300

    final exam

---
+++ js/app.js
@@ -0,0 +1,20 @@
+"use srtict"
+
+
+function Movie (MovieName, image, release) {
+    this.MovieName = Name;
+    this.image = image;
+    this.release = release;
+    
+  }
+
+var table = document.getElementById("myTable");
+
+
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+
+cell1.innerHTML = "NEW CELL1";
+cell2.innerHTML = "NEW CELL2";
+  
+  
